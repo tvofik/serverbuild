abfecd5900c205636e0275560b5d14eb84164ec6
Abayomi Pelewura <abayomi_pelewura@yahoo.co.uk>
2018-11-27 13:58:10 -0500

    Initial Commit

---
+++ DBconfig/dbconfig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Set up MySQL DB
+
+# Call the base config
+/bin/bash ../Baseconfig/baseconfig.sh
+
+# Install DB
+sudo yum install mariadb-server mariadb
+
+# Start the DB Service
+sudo systemctl start mariadb
+
+sudo mysql_secure_installation
+
+# Enable the sevice
+sudo systemctl enable mariadb.service
---
+++ Webappconfig/webappconfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script to create an admin user
+
+# Call the base config
+/bin/bash ../Baseconfig/baseconfig.sh
+
+# Update 
+sudo yum update -y
+
+# Install Apache webserver
+sudo yum install httpd -y
+
+# Start Apache Webserver
+systemctl start httpd
+
+# Enable Apache Webserver
+systemctl enable httpd 
